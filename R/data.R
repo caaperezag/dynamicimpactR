2eed6aa7ff4825a6b52a40b79c6bfa24c5ed0dde
caaperezag <caaperezag@outlook.com>
2022-08-24 07:23:04 -0500

    added data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Simulated input data.
+#'
+#'
+#' A matrix used as example in the package.
+#'
+#'
+#' @format a matrix of 122 rows and 2 columns:
+"X_matrix"
+
+
+#' Simulated response data.
+#'
+#'
+#' A matrix used as example in the package.
+#'
+#'
+#' @format a matrix of 122 rows and 2 columns:
+"Y_matrix"
